b7542a356e95506eaf904d06d8e05b3406ee7846
david.bashford <dbashford@bericotechnologies.com>
2014-03-05 07:17:13 -0500

    config to js, eslinting, leading comments removed

--- src/config.js
+++ src/config.js
@@ -11,16 +11,16 @@ exports.defaults = function() {
 };
 
 exports.placeholder = function() {
-  return "\t\n\n"+
-         "  # emblem:                  # config settings for the Emblem compiler module\n" +
-         "    # lib: undefined         # use this property to provide a specific version of Emblem\n" +
-         "    # extensions: [\"emblem\", \"embl\"]  # default extensions for Emblem files\n" +
-         "    # handlebars: undefined  # Emblem uses Handlebars during compilation, use this property\n" +
-         "                             # to provide a specific version of Handlebars for Emblem to use\n" +
-         "    # helpers:[\"app/template/handlebars-helpers\"]  # the paths from watch.javascriptDir to\n" +
-         "                             # the files containing handlebars helper/partial registrations\n" +
-         "    # emberPath: \"vendor/ember\" # AMD path for the Ember library, this is used as a\n" +
-         "                             # dependency in the compiled templates.\n";
+  return "\t\n\n" +
+         "  emblem:                  # config settings for the Emblem compiler module\n" +
+         "    lib: undefined         # use this property to provide a specific version of Emblem\n" +
+         "    extensions: [\"emblem\", \"embl\"]  # default extensions for Emblem files\n" +
+         "    handlebars: undefined  # Emblem uses Handlebars during compilation, use this property\n" +
+         "                           # to provide a specific version of Handlebars for Emblem to use\n" +
+         "    helpers:[\"app/template/handlebars-helpers\"]  # the paths from watch.javascriptDir to\n" +
+         "                           # the files containing handlebars helper/partial registrations\n" +
+         "    emberPath: \"vendor/ember\" # AMD path for the Ember library, this is used as a\n" +
+         "                           # dependency in the compiled templates.\n";
 };
 
 exports.validate = function( config, validators ) {
@@ -29,14 +29,14 @@ exports.validate = function( config, validators ) {
   if ( validators.ifExistsIsObject( errors, "emblem config", config.emblem ) ) {
 
     if ( !config.emblem.lib ) {
-      config.emblem.lib = require( 'emblem' );
+      config.emblem.lib = require( "emblem" );
     }
 
     if ( !config.emblem.handlebars ) {
-      config.emblem.handlebars = require( 'handlebars' );
+      config.emblem.handlebars = require( "handlebars" );
     }
 
-    var ec = require( './resources/ember-compiler' );
+    var ec = require( "./resources/ember-compiler" );
     config.emblem.handlebars = ec.makeHandlebars( config.emblem.handlebars );
 
     if ( validators.isArrayOfStringsMustExist( errors, "emblem.extensions", config.emblem.extensions ) ) {
